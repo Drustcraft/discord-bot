f2648c14f5bc30b310bfe2ff48487114614adc9d
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2022-05-09 16:04:39 +1000

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+const Discord = require("discord.js")
+const configJson = require('./config.json');
+const token = process.env.token;
+const clientId = process.env.clientId;
+const nl = require("npmlog");
+const baseCommandModulePath = "./lib/commands/"
+const fs = require("fs")
+
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
+
+if (configJson.versionInfo.enviornment == 0) {
+	nl.level = Infinity
+}
+
+nl.heading = "Drustcraft Bot"
+
+client.once('ready', () => {
+	client.user.setActivity('www.drustcraft.com.au', { type: 'WATCHING' });
+	nl.info('Base Bot', `Ready.`);
+	if (!configJson.versionInfo.noStartAlert) {
+		if (configJson.versionInfo.enviornment == 0) {
+			client.channels.cache.get('963968336847327355').send('Ready, running on development environment!')
+		} else {
+			client.channels.cache.get('963968336847327355').send('Ready, running on production environment!')
+		}
+	}
+});
+
+client.on('warn', (string) => {
+	nl.verbose("Discord.JS Warning", string)
+})
+
+client.on('debug', (string) => {
+	nl.verbose("Discord.JS Debug", string)
+})
+
+client.on('apiRequest', (apir) => {
+	nl.verbose("Discord.JS HTTP", apir.method)
+	nl.verbose("Discord.JS HTTP", apir.path)
+})
+
+client.on('error', (string) => {
+	nl.error("Discord.JS Error", string)
+})
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	await interaction.deferReply();
+	try {
+		if (fs.existsSync(`${baseCommandModulePath}${interaction.commandName}.js`)) {
+			const module = require(`${baseCommandModulePath}${interaction.commandName}.js`)
+			module.command(interaction)
+		} else {
+			const module = require(`${baseCommandModulePath}fallback.js`)
+			module.command(interaction)
+		}
+	} catch {
+		interaction.editReply("An error occurred while running this command.\It's most likely because the server is offline.")
+	}
+});
+
+client.login(token);
---
+++ regSlashCommands.js
@@ -0,0 +1,59 @@
+require('dotenv').config()
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const configJson = require('./config.json');
+const token = process.env.token;
+const clientId = process.env.clientId;
+const nl = require("npmlog");
+
+function errorFunc(error) {
+	nl.error("Discord API Down!", error)
+	process.exit(1)
+}
+
+nl.heading = "Drustcraft Bot"
+
+nl.info("Welcome!", "Registering Slash Commands...")
+
+nl.level = Infinity
+
+let commands = []
+
+for (i in configJson.commands) {
+	nl.verbose("Command Table" ,`Regisering command "${configJson.commands[i].name}" with description "${configJson.commands[i].description}".`)
+	commands.push(new SlashCommandBuilder().setName(configJson.commands[i].name).setDescription(configJson.commands[i].description))
+	if (configJson.commands[i].options != undefined) {
+		nl.verbose("Command Table" ,`"${configJson.commands[i].name}" has additional arguments that need to be processed.`)
+		for (v in configJson.commands[i].options) {
+			if (configJson.commands[i].options[v].type == 0) {
+				nl.verbose("Command Table" ,`String Argument "${configJson.commands[i].options[v].name}" has a description of "${configJson.commands[i].options[v].description}" and`)
+				if (configJson.commands[i].options[v].required) {
+					nl.verbose("Command Table" ,`is required.`)
+				} else {
+					nl.verbose("Command Table" ,`is not required.`)
+				}
+				commands[commands.length - 1].addStringOption(option => option.setName(configJson.commands[i].options[v].name).setDescription(configJson.commands[i].options[v].description).setRequired(configJson.commands[i].options[v].required))
+			}
+			if (configJson.commands[i].options[v].type == 1) {
+				nl.verbose("Command Table" ,`Boolean Argument "${configJson.commands[i].options[v].name}" has a description of "${configJson.commands[i].options[v].description}" and`)
+				if (configJson.commands[i].options[v].required) {
+					nl.verbose("Command Table" ,`is required.`)
+				} else {
+					nl.verbose("Command Table" ,`is not required.`)
+				}
+				commands[commands.length - 1].addBooleanOption(option => option.setName(configJson.commands[i].options[v].name).setDescription(configJson.commands[i].options[v].description).setRequired(configJson.commands[i].options[v].required))
+			}	
+		}
+	}
+}
+
+commands.map(command => command.toJSON());
+
+nl.info("Command Table", "Finished, sending to Discord.")
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationCommands(clientId), { body: commands })
+	.then(() => nl.info("Complete", 'Successfully registered application commands. Exiting.'))
+	.catch(errorFunc)
